2d984f61b18f682de3304fdb3a2649620df6fdfb
amylynncheng <amylynncheng@gmail.com>
2019-02-17 13:41:30 -0600

    Set up basic CalendarView

--- app/src/main/java/melochi/com/yearinpixels/CalendarActivity.java
+++ app/src/main/java/melochi/com/yearinpixels/CalendarActivity.java
@@ -1,13 +1,28 @@
 package melochi.com.yearinpixels;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class CalendarActivity extends AppCompatActivity {
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView mCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calendar);
+
+        mCalendarView = (CalendarView) findViewById(R.id.calendar_view);
+        mCalendarView.setOnDateChangeListener(new SelectDateListener());
+    }
+
+    private class SelectDateListener implements CalendarView.OnDateChangeListener {
+        @Override
+        public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+            Log.d(TAG, month + "/" + day + "/" + year);
+        }
     }
 }
