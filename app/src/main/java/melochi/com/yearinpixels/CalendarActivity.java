97ac7b573985f9aa10111b6c5046a695f3592b33
amylynncheng <amylynncheng@gmail.com>
2019-02-17 15:14:15 -0600

    Display selected date as string

--- app/src/main/java/melochi/com/yearinpixels/CalendarActivity.java
+++ app/src/main/java/melochi/com/yearinpixels/CalendarActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
     private static final String TAG = "CalendarActivity";
 
     private CalendarView mCalendarView;
+
+    String MONTHS[] = {"January", "February", "March", "April",
+            "May", "June", "July", "August", "September",
+            "October", "November", "December"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +21,13 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
 
         mCalendarView = (CalendarView) findViewById(R.id.calendar_view);
-        mCalendarView.setOnDateChangeListener(new SelectDateListener());
-    }
-
-    private class SelectDateListener implements CalendarView.OnDateChangeListener {
-        @Override
-        public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
-            Log.d(TAG, month + "/" + day + "/" + year);
-        }
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+                Log.d(TAG, month + "/" + day + "/" + year);
+                String selectedDate = MONTHS[month-1] + " " + day + ", " + year;
+                Toast.makeText(CalendarActivity.this, selectedDate, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
