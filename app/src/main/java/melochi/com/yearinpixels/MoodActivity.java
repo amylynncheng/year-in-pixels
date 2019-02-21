063484d6bfbc0c48d24da959e90e503cdd1cb8a8
amylynncheng <amylynncheng@gmail.com>
2019-02-21 16:15:47 -0600

    Refactor setDateTitle in MoodActivity

--- app/src/main/java/melochi/com/yearinpixels/MoodActivity.java
+++ app/src/main/java/melochi/com/yearinpixels/MoodActivity.java
@@ -11,10 +11,6 @@ import melochi.com.yearinpixels.constants.CalendarConstants;
 import melochi.com.yearinpixels.constants.Extras;
 
 public class MoodActivity extends Activity {
-    private TextView mDateTitle;
-
-    private Date selectedDate;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +18,22 @@ public class MoodActivity extends Activity {
 
         Bundle b = getIntent().getExtras();
         try {
-            selectedDate = (Date) b.get(Extras.DATE_SELECTED_EXTRA_KEY);
+            Date selectedDate = (Date) b.get(Extras.DATE_SELECTED_EXTRA_KEY);
+            setDateTitle(selectedDate);
         } catch (NullPointerException npe) {
             npe.printStackTrace();
         }
 
+
+    }
+
+    private void setDateTitle(Date selectedDate) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(selectedDate);
         String dateTitle = CalendarConstants.MONTH_NAMES[calendar.get(Calendar.MONTH)] + " "
                 + calendar.get(Calendar.DATE) + ", "
                 + calendar.get(Calendar.YEAR);
-
-        mDateTitle = findViewById(R.id.date_title);
+        TextView mDateTitle = findViewById(R.id.date_title);
         mDateTitle.setText(dateTitle);
     }
 }
