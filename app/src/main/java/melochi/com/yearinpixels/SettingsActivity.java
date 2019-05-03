7c6a1f1372cb58c50459aefb6c8862bfaae8abb3
amylynncheng <amylynncheng@gmail.com>
2019-05-03 11:00:28 -0500

    Add square views for each mood setting to preview current color palette

--- app/src/main/java/melochi/com/yearinpixels/SettingsActivity.java
+++ app/src/main/java/melochi/com/yearinpixels/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.larswerkman.holocolorpicker.ColorPicker;
@@ -21,6 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        initializeColorSquares();
 
         Button saveSettingsButton = findViewById(R.id.save_settings_button);
         saveSettingsButton.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +44,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void showColorPickerDialog(View v) {
         ImageButton selectedButton = (ImageButton) v;
+        RelativeLayout parent = (RelativeLayout) selectedButton.getParent();
 
         final Dialog dialog = new Dialog(SettingsActivity.this);
         dialog.setContentView(R.layout.color_picker_dialog);
@@ -58,7 +61,7 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
         Button saveButton = dialog.findViewById(R.id.save_color_button);
-        saveButton.setOnClickListener(new ColorSavedListener(selectedButton, picker, dialog));
+        saveButton.setOnClickListener(new ColorSavedListener(selectedButton, picker, dialog, parent));
         dialog.show();
     }
 
@@ -66,39 +69,66 @@ public class SettingsActivity extends AppCompatActivity {
         private ImageButton selectedButton;
         private ColorPicker colorPicker;
         private Dialog dialog;
+        private RelativeLayout moodLayout;
 
-        public ColorSavedListener(ImageButton button, ColorPicker picker, Dialog dialog) {
+        public ColorSavedListener(ImageButton button, ColorPicker picker, Dialog dialog, RelativeLayout parent) {
             this.selectedButton = button;
             this.colorPicker = picker;
             this.dialog = dialog;
+            this.moodLayout = parent;
         }
 
         @Override
         public void onClick(View view) {
             int colorChosen = colorPicker.getColor();
             ColorConstants palette = ColorConstants.get();
+            View colorSquare = null;
+
             switch (selectedButton.getId()) {
                 case (R.id.edit_great_color_button):
                     palette.setGreatColor(colorChosen);
+                    colorSquare = this.moodLayout.findViewById(R.id.great_color_square);
                     break;
                 case (R.id.edit_good_color_button):
                     palette.setGoodColor(colorChosen);
+                    colorSquare = this.moodLayout.findViewById(R.id.good_color_square);
                     break;
                 case (R.id.edit_okay_color_button):
                     palette.setOkayColor(colorChosen);
+                    colorSquare = this.moodLayout.findViewById(R.id.okay_color_square);
                     break;
                 case (R.id.edit_bad_color_button):
                     palette.setBadColor(colorChosen);
+                    colorSquare = this.moodLayout.findViewById(R.id.bad_color_square);
                     break;
                 case (R.id.edit_terrible_color_button):
                     palette.setTerribleColor(colorChosen);
+                    colorSquare = this.moodLayout.findViewById(R.id.terrible_color_square);
                     break;
                 default:
                     Toast.makeText(SettingsActivity.this,
                             "An error occurred",
                             Toast.LENGTH_SHORT).show();
             }
+            if (colorSquare != null) {
+                colorSquare.setBackgroundColor(colorChosen);
+            }
             dialog.dismiss();
         }
+    }
+
+    public void initializeColorSquares() {
+        int[] currentColors = ColorConstants.get().getPalette();
+
+        View terribleSquare = findViewById(R.id.terrible_color_square);
+        terribleSquare.setBackgroundColor(currentColors[0]);
+        View badSquare = findViewById(R.id.bad_color_square);
+        badSquare.setBackgroundColor(currentColors[1]);
+        View okaySquare = findViewById(R.id.okay_color_square);
+        okaySquare.setBackgroundColor(currentColors[2]);
+        View goodSquare = findViewById(R.id.good_color_square);
+        goodSquare.setBackgroundColor(currentColors[3]);
+        View greatSquare = findViewById(R.id.great_color_square);
+        greatSquare.setBackgroundColor(currentColors[4]);
     }
 }
